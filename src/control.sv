a9db1e31aa08c2b3c4c8f121858516f227a813cf
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2022-05-19 11:00:48 +0430

    implement control

--- src/control.sv
+++ src/control.sv
@@ -1,2 +1,42 @@
-module control();
+module control(
+    reg_dst,
+    alu_src,
+    mem_to_reg,
+    reg_write,
+    mem_read,
+    mem_write,
+    branch,
+    alu_op,
+    jump,
+    opcode
+);
+    parameter OPCODE_LENGTH = 6;
+    input [OPCODE_LENGTH-1:0] opcode;
+    output reg reg_dst,
+    output reg  [1:0] alu_src, // alu_src[0] => a, alu_src[1] => b
+    output reg mem_to_reg,
+    output reg reg_write,
+    output reg mem_read,
+    output reg mem_write,
+    output reg branch,
+    output reg alu_op,
+    output reg jump,
+    always_comb @(*) begin
+        reg_dst = 1'b0;
+        alu_src = 2'b00;
+        mem_to_reg = 1'b0;
+        reg_write = 1'b0;
+        mem_read = 1'b0;
+        mem_write = 1'b0;
+        branch = 1'b0;
+        alu_op = 1'b0;
+        jump = 1'b0;
+        case (opcode)
+            6'b001001: begin
+                alu_src = 1'b01;
+                reg_write = 1'b1;
+                alu_op = 1'b0;
+            end
+        endcase
+    end
 endmodule
--- src/mips_core.sv
+++ src/mips_core.sv
@@ -32,6 +32,24 @@ module mips_core(
     wire             clk;
     wire             rst_b;
     wire             halted;
+    wire             reg_dst;
+    wire             alu_src;
+    wire             mem_to_reg;
+    wire             reg_write;
+    wire             mem_read;
+    wire             mem_write;
+    wire             branch;
+    wire             alu_op;
+    wire             jump;
+    wire [5:0]       opcode;
+    wire             control;
+    wire [5:0]       func;
+    wire [31:0]      alu_result;
+    wire             zero;
+    wire [31:0]      a;
+    wire [31:0]      b;
+
+    reg [31:0] pc;
 
     regfile regfile(
         .rs_data(rs_data),
@@ -45,6 +63,46 @@ module mips_core(
         .rst_b(rst_b),
         .halted(halted)
     );
-    
+    control control(
+        .reg_dst(reg_dst),
+        .alu_src(alu_src),
+        .mem_to_reg(mem_to_reg),
+        .reg_write(reg_write),
+        .mem_read(mem_read),
+        .mem_write(mem_write),
+        .branch(branch),
+        .alu_op(alu_op),
+        .jump(jump),
+        .opcode(opcode)
+    );
 
+    alu_control alu_control(
+        .control(control),
+        .alu_op(alu_op),
+        .func(func)
+    );
+    
+    alu alu(
+        .alu_result(alu_result),
+	    .zero(zero),
+	    .a(a), 
+        .b(b),
+        .control(control)
+    );
+
+    assign immediate_data = inst[15:0];
+    assign sh_amount = inst[11:6]; 
+    assign opcode = inst[31:26];
+    assign func = inst[5:0];
+    assign sign_extend_immediate = {16{immediate_data[15]}, immediate_data};
+    assign a = (alu_src[0] == 1'b1) ? sh_amount : rs_data;
+    assign b = (alu_src[1] == 1'b1) ? sign_extend_immediate : rt_data;
+
+    assign next_pc = pc + 4;
+    assign inst_addr = pc;
+    assign halted = (opcode == 0 && func == 6'b001100) ? 1'b1 : 1'b0;
+
+    always_ff @(posedge clock) begin
+        pc <= next_pc;
+    end
 endmodule
