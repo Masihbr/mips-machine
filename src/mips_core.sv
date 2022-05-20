5765b9d39ba62077e366e329fb34ad2aec670edc
masihbr <masihbr@gmail.com>
2022-05-20 13:27:48 +0430

    Extract pc_control combinational logic

--- src/mips_core.sv
+++ src/mips_core.sv
@@ -22,12 +22,12 @@ module mips_core(
 
     parameter XLEN=32;
 
-    wire [XLEN-1:0] rs_data;
-    wire [XLEN-1:0] rt_data;
-    wire       [4:0] rs_num;
-    wire       [4:0] rt_num;
-    wire       [4:0] rd_num;
-    wire  [XLEN-1:0] rd_data;
+    wire [XLEN-1:0]  rs_data;
+    wire [XLEN-1:0]  rt_data;
+    wire [4:0]       rs_num;
+    wire [4:0]       rt_num;
+    wire [4:0]       rd_num;
+    wire [XLEN-1:0]  rd_data;
     wire             clk;
     wire             rst_b;
     wire             halted;
@@ -52,10 +52,10 @@ module mips_core(
     wire [15:0]      immediate_data;
     wire [4:0]       sh_amount;
     wire [31:0]      sign_extend_immediate;
-    wire [25:0]     address;
-    reg [31:0]       next_pc;
-    reg [31:0]       pc;
-    reg [31:0]       pc4;
+    wire [25:0]      address;
+    wire [31:0]      next_pc;
+    reg  [31:0]      pc;
+    reg  [31:0]      pc4;
 
 
     regfile regfile_unit(
@@ -101,6 +101,18 @@ module mips_core(
         .control(control)
     );
 
+    pc_control pc_control(
+        .next_pc(next_pc),
+        .pc(pc),
+        .address(address),
+        .jump(jump),
+        .branch(branch),
+        .jr(jr),
+        .zero(zero),
+        .sign_extend_immediate(sign_extend_immediate),
+        .rs_data(rs_data)
+    );
+
     assign rs_num = inst[25:21];
     assign rt_num = inst[20:16];
     assign rd_num = (reg_dst == 1'b1) ? inst[15:11] : (jump == 2'b10) ? 5'd31 : rt_num;
@@ -116,57 +128,12 @@ module mips_core(
     assign b = (alu_src[1] == 1'b1) ? sign_extend_immediate : rt_data;
 
     assign address = inst[25:0];
-    assign pc4 = pc + 4;
 
     assign inst_addr = pc;
     assign halted = (opcode == 0 && func == 6'b001100) ? 1'b1 : 1'b0;
 
     assign mem_write_en = mem_write;
     assign mem_addr = alu_result;
-
-
-    always_comb begin
-        next_pc = pc4;
-        if (jump != 2'b00) begin
-            next_pc = {pc4[31:28] ,address, 2'b00}; 
-        end 
-        else if (jr == 1'b1) begin
-            next_pc = rs_data;
-        end
-        else begin
-            case(branch)
-                3'b100: begin
-                    if (zero == 1'b1) begin
-                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
-                    end
-                end
-                3'b101: begin
-                    if (zero == 1'b0) begin
-                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
-                    end
-                end 
-
-                3'b110: begin
-                    if ($signed(rs_data) <= 0) begin
-                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
-                    end
-                end
-
-                3'b111: begin
-                    if ($signed(rs_data) > 0) begin
-                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
-                    end
-                end
-
-                3'b001: begin
-                    if ($signed(rs_data) >= 0) begin
-                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
-                    end
-                end
-                default: next_pc = pc4;
-            endcase
-        end
-    end
 
     always_ff @(posedge clk) begin
         $display("inst=%b\npc=%d\na=%b\nb=%b\nalu_res=%b\nrd_data=%b\nalu_src=%b\nim=%b\nrd_num=%b\ncontrol=%d\nalu_op=%b\nfunc=%b\nnext_pc=%d\n-------------------------------------",
---
+++ src/pc_control.sv
@@ -0,0 +1,70 @@
+module pc_control(
+    next_pc,
+    pc,
+    address,
+    jump,
+    branch,
+    jr,
+    zero,
+    sign_extend_immediate,
+    rs_data
+);
+    parameter N = 32;
+    output reg  [N-1:0]      next_pc;
+    input [N-1:0]            pc;
+    input [25:0]             address;
+    input [1:0]              jump;
+    input [2:0]              branch;
+    input                    jr;
+    input                    zero;  
+    input [N-1:0]            sign_extend_immediate;
+    input [N-1:0]            rs_data;
+
+    wire [N-1:0] pc4;
+    assign pc4 = pc + 32'd4;
+
+    always_comb begin   
+        next_pc = pc4;
+        if (jump != 2'b00) begin
+            next_pc = {pc4[31:28] ,address, 2'b00}; 
+        end 
+        else if (jr == 1'b1) begin
+            next_pc = rs_data;
+        end
+        else begin
+            case(branch)
+                3'b100: begin
+                    if (zero == 1'b1) begin
+                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
+                    end
+                end
+
+                3'b101: begin
+                    if (zero == 1'b0) begin
+                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
+                    end
+                end 
+
+                3'b110: begin
+                    if ($signed(rs_data) <= 0) begin
+                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
+                    end
+                end
+
+                3'b111: begin
+                    if ($signed(rs_data) > 0) begin
+                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
+                    end
+                end
+
+                3'b001: begin
+                    if ($signed(rs_data) >= 0) begin
+                        next_pc = pc4 + {sign_extend_immediate[29:0],2'b00};
+                    end
+                end
+                default: next_pc = pc4;
+            endcase
+        end
+    end
+
+endmodule
