a3e06582ca490c20ad8de8d28fd154de88a1ccb2
masihbr <masihbr@gmail.com>
2022-05-23 11:41:30 +0430

    Add slti

--- src/control.sv
+++ src/control.sv
@@ -41,62 +41,67 @@ module control(
         jump = 2'b00;
         /* verilator lint_off CASEX */
         casex (opcode)
-            6'b000000: begin
+            6'b000000: begin // I-type
                 alu_src = 2'b00;
                 reg_write = 1'b1;
                 reg_dst = 1'b1;
                 case (func)
-                    6'b000000, 6'b000010, 6'b000011: begin
+                    6'b000000, 6'b000010, 6'b000011: begin // shifts
                     alu_src[0] = 1'b1; // use sh_amount
                     alu_op = 4'b0000;
                     end
-                    6'b001000: begin
+                    6'b001000: begin // jr
                         jr = 1'b1;
                     end
                     default: begin
                     end
                 endcase
             end
-            6'b001000: begin //ADDi
+            // R-type - J-type
+            6'b001000: begin // ADDi
                 alu_src = 2'b10;
                 reg_write = 1'b1;
                 alu_op = 4'b0001;
             end
-            6'b001001: begin //ADDiu
+            6'b001001: begin // ADDiu
                 alu_src = 2'b10;
                 reg_write = 1'b1;
                 alu_op = 4'b0010;
                 do_extend = 1'b0;
             end
-            6'b001100: begin //ANDi
+            6'b001100: begin // ANDi
                 alu_src = 2'b10;
                 reg_write = 1'b1;
                 alu_op = 4'b0011;
                 do_extend = 1'b0;
             end
-            6'b001110: begin //XORi
+
+            6'b001110: begin // XORi
                 alu_src = 2'b10;
                 reg_write = 1'b1;
                 alu_op = 4'b0100;
                 do_extend = 1'b0;
             end
-            6'b001101: begin// ORi
+            
+            6'b001101: begin // ORi
                 alu_src = 2'b10;
                 reg_write = 1'b1;
                 do_extend = 1'b0;
                 alu_op = 4'b0101;
             end
 
-            6'b0001xx: begin //BEQ, BNE, BLEZ, BGTZ
+            6'b0001xx: begin // BEQ, BNE, BLEZ, BGTZ
                 alu_src = 2'b00;
                 branch = opcode[2:0];
                 alu_op = 4'b1000;
             end
-            6'b000001: begin //BGEZ
+
+            6'b000001: begin // BGEZ
                 alu_src = 2'b00;
                 branch = 3'b001;
             end
-            6'b001111: begin// Lui
+            
+            6'b001111: begin // Lui
                 alu_src = 2'b10;
                 reg_write = 1'b1;
                 alu_op = 4'b0111;
@@ -106,7 +111,7 @@ module control(
                 jump = 2'b01;
             end
 
-            6'b000011: begin //jal
+            6'b000011: begin // jal
                 jump = 2'b10;
                 reg_write = 1'b1;
             end
@@ -127,6 +132,12 @@ module control(
                 
             end
             
+            6'b001010: begin // SLTi
+                alu_src = 2'b10;
+                alu_op = 4'b0101;
+                reg_write = 1'b1;
+            end
+
             default: begin
                 alu_src = 2'b00;
             end
