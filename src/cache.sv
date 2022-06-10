74feb41316e456dc5810544d3bd61898651d6d29
masihbr <masihbr@gmail.com>
2022-06-10 12:24:17 +0430

    Add more input outputs

--- src/cache.sv
+++ src/cache.sv
@@ -2,28 +2,41 @@
 
 
 module cache(
-    
+    hit,
     cache_data_out,
-    rst_b,
+    mem_data_in,
+    mem_write_en,
+    address,
+    cache_data_in,
+    mem_data_out,
+    cache_write_en,
     clk,
-    cache_addr,
-    cache_hit
-
+    rst_b
 );
 
-    output [31:0] cache_data_out;
-    output cache_hit;
-    input rst_b;
-    input clk;
-    input [31:0] cache_addr;
+    output [31:0]  cache_data_out;
+    output [31:0]  mem_data_in;
+    output         mem_write_en;
+    output         hit;
 
-    parameter start = 0, top = (1 << 12) - 1;
+    input  [31:0]  mem_data_out;
+    input  [31:0]  cache_data_in;
+    input          cache_write_en;
+    input          rst_b;
+    input          clk;
+    input  [31:0]  address;
 
-    reg [31:0] data[start:top];
-    reg valid[start:top];
-    reg [1:0] tag_array[start:top];
+    parameter blocks_number = 12;
+    parameter word_size = 32; 
+    parameter tag_size = 16 - blocks_number - 2;
+    parameter start = 0, top = (1 << blocks_number) - 1;
 
+    reg  [word_size-1:0]        data[start:top];
+    reg                         valid[start:top];
+    reg  [tag_size-1:0]         tag_array[start:top];
+    wire [blocks_number-1:0]    ea;
 
+    assign ea = address[blocks_number + 2 : -blocks_number];
 
     always_ff @(posedge clk, negedge rst_b) begin
         if (rst_b == 0) begin
@@ -35,9 +48,4 @@ module cache(
             end
         end
     end
-
-
- 
-
-
 endmodule
